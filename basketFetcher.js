7dd8613d45022ef634d4bc4b0c6a7fb9597fb577
Lnac <helena.domlan@gmail.com>
2021-05-18 22:27:46 +0200

    ajout de la searchBar sur l'ensemble des pages EllES avec actualisation du style css prenant en compte la searchBar

--- basketFetcher.js
+++ basketFetcher.js
@@ -1,34 +1,29 @@
 $(function() {
 
   let basketProducts = [];
-  // counteur de totalités de produits ajoutés
-  var counter = 0;
+
 
 //recupere le panier grace a storage
   if(localStorage.getItem("basketProducts") != null){
     basketProducts =  JSON.parse(localStorage.getItem("basketProducts"));
-  } 
-  // recupere le counter depuis storage
-  if(localStorage.getItem("counter") != null){
-    counter =  localStorage.getItem("counter");
- } 
+  }
+
 //liste view
   listProductViewComponent = '';
   basketProducts.forEach( product =>
- 
-    listProductViewComponent += `<tr> 
-                       <td> 
-                          <img src= ${product.pic} class="img-fluid w-50 d-block mx-auto" > 
-                          </td> 
-                          <td> 
+    listProductViewComponent += `<tr>
+                       <td>
+                          <img src= ${product.pic} class="img-fluid w-50 d-block mx-auto" >
+                          </td>
+                          <td>
                           <p class="text-muted small text-center">${product.name}<br> Brand: Andrew</p>
-                        </td> 
-                        <td> 
-                        <p class="price text-center">${product.price}€</p> 
-                        </td> 
-                        <td> 
+                        </td>
+                        <td>
+                        <p class="price text-center">${product.price}€</p>
+                        </td>
+                        <td>
                           <p class="text-muted small text-center cart-quantity-input">${product.quantity} <br> Brand: Andrew</p>
-                        </td> 
+                        </td>
                         <td class="text-right d-block">
                         <p  class="btn btn-danger btn-round" data-id = ${product.id} id='btn-delet' > Supprimer</p>
                         </td>
@@ -37,7 +32,8 @@ $(function() {
  $('#productListViewId').html(listProductViewComponent)
 
  calculatTotalPriceAndDisplay()
- 
+ computeTotalQuantityAndDisplay()
+
    function calculatTotalPriceAndDisplay() {
     let total = calculTotalPrice();
     displayPrice(total + " €");
@@ -49,14 +45,35 @@ $(function() {
     basketProducts.forEach( product =>
       total = total + (product.quantity * product.price)
       )
-     return  Math.round(total)
+//     return  Math.round(total)
+       return (total.toFixed(2));  // renvoie 2 chiffres après la virgule
+//     return ((parseInt(total * 100)) / 100);
+//     return (Math.floor(total * 100) / 100);
    }
 
    function displayPrice(value) {
     $('#total').html(value)
     console.log(value)
    }
- 
+
+   function computeTotalQuantityAndDisplay() {
+    let totalQuantity = computeTotalQuantity();
+    displayTotalQuantity(totalQuantity);
+    console.log("computeTotalQuantityAndDisplayFetcher")
+   }
+
+   function computeTotalQuantity() {
+     let totalQuantity = 0
+     basketProducts.forEach( product =>
+      totalQuantity = totalQuantity + product.quantity
+      )
+      return  totalQuantity
+   }
+   function displayTotalQuantity(value) {
+    $('.basketIcon').html(value)
+    console.log(value)
+   }
+
 
 function addRemoveProductListeners(){
   //ON cible le button avec le classe "btn-danger" pour supprimer les produits
@@ -65,11 +82,11 @@ function addRemoveProductListeners(){
   for(var i = 0; i < removeButtons.length; i++){
   var button = removeButtons[i]
   button.addEventListener('click', removeBasketProduct)
-  } 
+  }
  }
 //La fonction qui supprime les produits du panier
 function removeBasketProduct(event){
-  
+
   var buttonClicked = event.target
   // on recupere le data-id qui represente l id du produit cliqué
   let productId = $(buttonClicked).attr("data-id")
@@ -79,12 +96,10 @@ function removeBasketProduct(event){
   });
 
   if(index > -1){
-// on met a jour le conteur de la quantité total du produit
-    counter = counter - basketProducts[index].quantity
     // on supprime le produit selectionne de la liste
     basketProducts.splice(index, 1)
-// mettre a jour le storage pour que les autres paye ayent les meme quantité 
-    localStorage.setItem("counter", counter)
+    // on met a jour le conteur de la quantité total du produit
+    computeTotalQuantityAndDisplay()
     localStorage.setItem("basketProducts", JSON.stringify(basketProducts));
   }
 
@@ -93,16 +108,8 @@ function removeBasketProduct(event){
   buttonClicked.parentElement.parentElement.remove()
 
 }
+
   })
 
- 
+
   
-
-
-
-
-
-
-
-
-
